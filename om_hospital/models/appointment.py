eef8c31e64ec2ce60427a500d4f6dc6f5c2dc55a
DESKTOP\Administrator <wankaho@hotmail.com>
2024-10-11 23:12:50 +0800

    add a sequence no. for appointment reference odoo will auto increase the value

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -10,6 +10,15 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
     _rec_name = 'patient_id'
 
+    reference = fields.Char(string="Reference", default="New")
     patient_id = fields.Many2one('hospital.patient', string="Patient")
     date_appointment = fields.Date(string="Date")
     note = fields.Text(string="Note")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        print("odoo mates", vals_list)
+        for vals in vals_list:
+            if not vals.get('reference') or vals['reference'] == 'New':
+                vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment')
+        return super().create(vals_list)
