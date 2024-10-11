223a4dc51d71d15ab2569da3fc1ef7ae85e39cef
DESKTOP\Administrator <wankaho@hotmail.com>
2024-10-11 20:54:53 +0800

    fix selection brackets

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -10,4 +10,4 @@ class HospitalPatient(models.Model):
 
     name = fields.Char(string="Name", required=True)
     date_of_birth = fields.Date(string="DOB")
-    gender = fields.Selection([{'male', 'Male'}, {'female', 'Female'}], string="Gender")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
