268a891c6f2204f2a35e50db779a1c8fd7ba5c02
DESKTOP\Administrator <wankaho@hotmail.com>
2024-10-11 20:23:30 +0800

    added U.I. views

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -2,5 +2,9 @@
     "name": "Hospital Management System",
     "author": "Jimmy the Developer",
     "license": "LGPL-3",
-    "version": "17.0.1.1"
+    "version": "17.0.1.1",
+    "data": [
+        "views/patient_views.xml",
+        "views/menu.xml"
+    ]
 }
