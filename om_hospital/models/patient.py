0a9b0c26c8df504cbfdd894aa5eb012ce2100f35
DESKTOP\Administrator <wankaho@hotmail.com>
2024-10-11 20:05:38 +0800

    created patient model

---
+++ om_hospital/models/patient.py
@@ -0,0 +1,13 @@
+from odoo import api, fields, models
+
+
+class HospitalPatient(models.Model):
+    # 6 fields are automatically created
+    # create_date, create_uid, display_name
+    # id, write_date, write_uid
+    _name = 'hospital.patient'
+    _description = 'Patient Master'
+
+    name = fields.Char(string="Name", required=True)
+    date_of_birth = fields.Date(string="DOB")
+    gender = fields.Selection([{'male', 'Male'}, {'female', 'Female'}], string="Gender")
