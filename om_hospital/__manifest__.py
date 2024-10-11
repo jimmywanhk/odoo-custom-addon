3d66ad9dd078c4b5c7f5149157e109e67697750c
DESKTOP\Administrator <wankaho@hotmail.com>
2024-10-11 22:16:36 +0800

    enable the audit log tracking function (chatter) for patient model

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -3,6 +3,9 @@
     "author": "Jimmy the Developer",
     "license": "LGPL-3",
     "version": "17.0.1.1",
+    "depends": [
+        'mail'
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/patient_views.xml",
--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -6,8 +6,9 @@ class HospitalPatient(models.Model):
     # create_date, create_uid, display_name
     # id, write_date, write_uid
     _name = 'hospital.patient'
+    _inherit = ['mail.thread']
     _description = 'Patient Master'
 
-    name = fields.Char(string="Name", required=True)
-    date_of_birth = fields.Date(string="DOB")
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender")
+    name = fields.Char(string="Name", required=True, tracking=True)
+    date_of_birth = fields.Date(string="DOB", tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string="Gender", tracking=True)
