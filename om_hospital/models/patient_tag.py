f1e470cddd1a9a0afc2f56c9de39f95956f405d3
伯伯後\Administrator <wankaho@hotmail.com>
2024-10-14 11:58:05 +0800

    added a new field - sequence sequence can be used to order the option items 'handle' widget can be used to enable drag and drop function to change the order

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -7,5 +7,7 @@ class PatientTag(models.Model):
     # id, write_date, write_uid
     _name = 'patient.tag'
     _description = 'Patient Tag'
+    _order = 'sequence,id'
 
     name = fields.Char(string="Name", required=True)
+    sequence = fields.Integer(default=10)
